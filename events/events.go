296a50a1a985db2f525fa5df46fb84f5353a220e
simenghe <simengschool@gmail.com>
2023-01-01 10:32:41 -0500

    sqlite

--- events/events.go
+++ events/events.go
@@ -4,7 +4,13 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type EventData struct {
+	Database *sqlx.DB
+}
 
 type EventPublisher struct {
 	Rate     time.Duration
@@ -13,6 +19,27 @@ type EventPublisher struct {
 
 func CreateEventPublisher(rate time.Duration) *EventPublisher {
 	return &EventPublisher{Rate: rate, Messages: make(chan string)}
+}
+
+func CreateEventData() *EventData {
+	db, err := sqlx.Connect("sqlite3", "database.db")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec(
+		`CREATE TABLE users (
+		id INTEGER PRIMARY KEY,
+		username TEXT NOT NULL,
+		password TEXT NOT NULL);`)
+
+	if err != nil {
+		panic(err)
+	}
+	return &EventData{}
+}
+
+func StartDatabase() {
+
 }
 
 func (e *EventPublisher) Poll() <-chan string {
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module go-ds
 go 1.19
 
 require github.com/google/uuid v1.3.0
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+)
